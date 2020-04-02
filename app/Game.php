4e9aa961cf0df875125479c6acf18edaca69266b
artem-brezutsky <a.brezutsky@gmail.com>
2020-04-02 16:44:39 +0300

    setup model

--- app/Game.php
+++ app/Game.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'complexity', 'minPlayers', 'maxPlayers', 'isActive'];
+
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
 }
