49bea1ea7197eb4aeb6adcb2d217c48b7fc9e355
artem-brezutsky <a.brezutsky@gmail.com>
2020-04-02 17:03:01 +0300

    refactoring

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use App\Http\Controllers\GameController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::apiResource('/games', 'GameController');
